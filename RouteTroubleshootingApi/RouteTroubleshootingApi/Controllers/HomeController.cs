14e452b6374814f482115ae1d32ca18169c676d5
André Luiz Soares Alves Monteiro <andremonteiro.cb@hotmail.com>
2022-06-15 09:25:08 -0300

    subindo projeto

---
+++ RouteTroubleshootingApi/RouteTroubleshootingApi/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RouteTroubleshootingApi.Models;
+using System.Diagnostics;
+
+namespace RouteTroubleshootingApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["APP_NAME"] = _configuration.GetValue<string>("APP_NAME");
+            ViewData["MACHINE_NAME"] = Environment.MachineName;
+            ViewData["REQUEST_HOST"] = Request.Host.Value;
+            ViewData["REQUEST_PATH"] = Request.Path;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RouteTroubleshootingApi/RouteTroubleshootingApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RouteTroubleshootingApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
